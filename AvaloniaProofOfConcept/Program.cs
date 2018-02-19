7891e348e1e813587c85b02c3df997abdb260b75
Peter Vietense <peter.vietense@hotmail.de>
2018-02-19 18:17:05 +0100

    init basic Hello World

---
+++ AvaloniaProofOfConcept/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using AvaloniaProofOfConcept.ViewModels;
+using AvaloniaProofOfConcept.Views;
+
+namespace AvaloniaProofOfConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
